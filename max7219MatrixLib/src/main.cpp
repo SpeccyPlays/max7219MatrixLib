3b222c5fb15a3dd6286b4dd69bfef38c00f6e221
SpeccyPlays <102996742+SpeccyPlays@users.noreply.github.com>
2022-11-20 17:20:01 +0000

    Finished the megaman sprite

--- max7219MatrixLib/src/main.cpp
+++ max7219MatrixLib/src/main.cpp
@@ -1,43 +1,41 @@
 #include <Arduino.h>
 #include "LedMatrix.h"
 #include "sprites.h"
-
-
-
 LedMatrix matrix(4, 3);
-uint16_t delayValue = 10;
+uint16_t delayValue = 150;
 byte count = 0;
 void graphicsDemo();
 void manicMinerSprites();
-
-;
+const byte *megaMan[6] ={megaMan1, megaMan2, megaMan3, megaMan4, megaMan5, megaMan6};
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   matrix.init();
 }
-
 void loop() {
-  delayValue = 100;
-  
-  matrix.wipeScreenBuffer();
-  for (byte x = 0; x <= 16; x += 8){
-    matrix.drawCustomSizeArray(x, 0, megaMan1, x * 3, 24);
+  //go through all the sprites
+  for (byte i = 0; i < sizeof(megaMan)/sizeof(megaMan[0]); i++){
+    matrix.wipeScreenBuffer();
+    for (byte x = 0; x <= 16; x += 8){
+      //the arrays are 72 columns
+      //megaman is 24 x 24 pixels
+      //the arrays are displayed in 3 chunks of 24
+      //the each chunk is 8 pixels across from the last
+      matrix.drawCustomSizeArray(x + 4, 0, megaMan[i], x * 3, 24);
+    }
+    matrix.sendScreenBuffer();
+    delay(delayValue);
   }
-  matrix.sendScreenBuffer();
-  delay(delayValue);
-  matrix.wipeScreenBuffer();
-  for (byte x = 0; x <= 16; x += 8){
-    matrix.drawCustomSizeArray(x, 0, megaMan2, x * 3, 24);
+  //just go through the running sprites
+  for (byte loop = 0; loop < 6; loop++){
+    for (byte i = 3; i < sizeof(megaMan)/sizeof(megaMan[0]); i++){
+      matrix.wipeScreenBuffer();
+      for (byte x = 0; x <= 16; x += 8){
+        matrix.drawCustomSizeArray(x + 4, 0, megaMan[i], x * 3, 24);
+      }
+      matrix.sendScreenBuffer();
+      delay(delayValue);
+    }
   }
-  matrix.sendScreenBuffer();
-  delay(delayValue);
-  matrix.wipeScreenBuffer();
-  for (byte x = 0; x <= 16; x += 8){
-    matrix.drawCustomSizeArray(x, 0, megaMan3, x * 3, 24);
-  }
-  matrix.sendScreenBuffer();
-  delay(delayValue);
 }
 void graphicsDemo(){
   matrix.wipeScreenBuffer();
@@ -79,7 +77,7 @@ void graphicsDemo(){
 void manicMinerSprites(){
 matrix.wipeScreenBuffer();
   if (count == 0){
-      matrix.draw16BitArray(4, 0, trumpetFacel);
+    matrix.draw16BitArray(4, 0, trumpetFacel);
     matrix.draw16BitArray(12, 0, trumpetFacer);
     count ++;
   }
@@ -88,7 +86,6 @@ matrix.wipeScreenBuffer();
     matrix.draw16BitArray(12, 0, trumpetFace2r);
     count = 0;
   }
-
   matrix.draw16BitArray(14, 8, willySprite);
   matrix.sendScreenBuffer();
   delayValue = 200;
